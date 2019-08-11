40849f6c5cc4b660a0d5f6a350f38355b750ad72
Antonppavlov <anton.it.pavlov@gmail.com>
2019-08-11 12:13:12 +0300

    разбил общий класс WebSteps.java на классы по действиям и проверкам

---
+++ at-library-web/src/main/java/ru/bcs/at/library/core/steps/WebCheckSteps.java
@@ -0,0 +1,408 @@
+package ru.bcs.at.library.core.steps;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.ex.ElementShould;
+import cucumber.api.java.ru.Когда;
+import cucumber.api.java.ru.Тогда;
+import ru.bcs.at.library.core.cucumber.api.CoreScenario;
+
+import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.WebDriverRunner.isIE;
+import static org.junit.Assert.*;
+import static ru.bcs.at.library.core.steps.OtherSteps.*;
+import static ru.bcs.at.library.core.steps.WebStepConfig.DEFAULT_TIMEOUT;
+
+/**
+ * <h1 style="color: green; font-size: 2.2em">
+ * WEB шаги
+ * </h1>
+ *
+ * <p style="color: green; font-size: 1.5em">
+ * В coreScenario используется хранилище переменных. Для сохранения/изъятия переменных используются методы setVar/getVar
+ * Каждая страница, с которой предполагается взаимодействие, должна быть описана в соответствующем классе,
+ * наследующем CorePage. Для каждого элемента следует задать имя на русском, через аннотацию @Name, чтобы искать
+ * можно было именно по русскому описанию, а не по селектору. Селекторы следует хранить только в классе страницы,
+ * не в степах, в степах - взаимодействие по русскому названию элемента.</p>
+ */
+public class WebCheckSteps {
+    private CoreScenario coreScenario = CoreScenario.getInstance();
+
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка появления элемента(не списка) на странице в течение DEFAULT_TIMEOUT.
+     * В случае, если свойство "waitingCustomElementsTimeout" в application.properties не задано,
+     * таймаут равен 10 секундам
+     *
+     * @param elementName название кнопки|поля|блока
+     *                    </p>
+     */
+    @Тогда("^элемент \"([^\"]*)\" отображается на странице$")
+    public void elemIsPresentedOnPage(String elementName) {
+        coreScenario.getCurrentPage().waitElementsUntil(
+                Condition.appear, DEFAULT_TIMEOUT, coreScenario.getCurrentPage().getElement(elementName)
+        );
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка появления элемента(не списка) на странице в течение
+     * заданного количества секунд
+     *
+     * @param elementName название кнопки|поля|блока
+     * @param seconds     количество секунд
+     *                    </p>
+     */
+    @Тогда("^элемент \"([^\"]*)\" отобразился на странице в течение (\\d+) (?:секунд|секунды)")
+    public void testElementAppeared(String elementName, int seconds) {
+        coreScenario.getCurrentPage().waitElementsUntil(
+                Condition.appear, seconds * 1000, coreScenario.getCurrentPage().getElement(elementName)
+        );
+    }
+
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка того, что элемент исчезнет со страницы (станет невидимым) в течение DEFAULT_TIMEOUT.
+     * В случае, если свойство "waitingCustomElementsTimeout" в application.properties не задано,
+     * таймаут равен 10 секундам
+     *
+     * @param elementName название кнопки|поля|блока
+     *                    </p>
+     */
+    @Тогда("^ожидается исчезновение элемента \"([^\"]*)\"")
+    public void elemDisappered(String elementName) {
+        coreScenario.getCurrentPage().waitElementsUntil(
+                Condition.disappears, DEFAULT_TIMEOUT, coreScenario.getCurrentPage().getElement(elementName));
+    }
+
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка того, что значение из поля совпадает со значением заданной переменной из хранилища
+     * </p>
+     *
+     * @param elementName  название поля|элемента
+     * @param variableName имя переменной
+     */
+    @Тогда("^значение (?:поля|элемента) \"([^\"]*)\" совпадает со значением из переменной \"([^\"]*)\"$")
+    public void compareFieldAndVariable(String elementName, String variableName) {
+        SelenideElement element = coreScenario.getCurrentPage().getElement(elementName);
+        String expectedValue = coreScenario.getVar(variableName).toString();
+        element.shouldHave(exactText(expectedValue));
+    }
+
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка того, что блок исчез/стал невидимым
+     * </p>
+     */
+    @Тогда("^(?:страница|блок|форма) \"([^\"]*)\" (?:скрыт|скрыта)")
+    public void blockDisappeared(String nameOfPage) {
+        if (isIE()) {
+            coreScenario.getPage(nameOfPage).ieDisappeared();
+        } else coreScenario.getPage(nameOfPage).disappeared();
+    }
+
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что поле для ввода пусто
+     * </p>
+     */
+    @Тогда("^поле \"([^\"]*)\" пусто$")
+    public void fieldInputIsEmpty(String fieldName) {
+        coreScenario.getCurrentPage().getElement(fieldName)
+                .shouldHave(empty);
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Сохранение значения элемента в переменную
+     * </p>
+     */
+    @Когда("^значение (?:элемента|поля) \"([^\"]*)\" сохранено в переменную \"([^\"]*)\"$")
+    public void storeElementValueInVariable(String elementName, String variableName) {
+        coreScenario.setVar(variableName, coreScenario.getCurrentPage().getAnyElementText(elementName));
+        coreScenario.write("Значение [" + coreScenario.getCurrentPage().getAnyElementText(elementName) + "] сохранено в переменную [" + variableName + "]");
+    }
+
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что элемент на странице кликабелен
+     * </p>
+     */
+    @Тогда("^(?:поле|элемент) \"([^\"]*)\" кликабельно$")
+    public void clickableField(String elementName) {
+        SelenideElement element = coreScenario.getCurrentPage().getElement(elementName);
+        element.shouldHave(enabled);
+    }
+
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что элемент на странице кликабелен
+     * </p>
+     */
+    @Тогда("^(?:поле|элемент) \"([^\"]*)\" кликабельнов течение (\\d+) (?:секунд|секунды)$")
+    public void clickableField(String elementName, int second) {
+        SelenideElement element = coreScenario.getCurrentPage().getElement(elementName);
+        element.waitUntil(enabled, second * 1000);
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что у элемента есть атрибут с ожидаемым значением (в приоритете: из property, из переменной сценария, значение аргумента)
+     * </p>
+     */
+    @Тогда("^элемент \"([^\"]*)\" содержит атрибут \"([^\"]*)\" со значением \"(.*)\"$")
+    public void checkElemContainsAtrWithValue(String elementName, String attribute, String expectedAttributeValue) {
+        expectedAttributeValue = getPropertyOrStringVariableOrValue(expectedAttributeValue);
+        SelenideElement currentElement = coreScenario.getCurrentPage().getElement(elementName);
+        currentElement.shouldHave(attribute(attribute, expectedAttributeValue));
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что элемент содержит указанный класс (в приоритете: из property, из переменной сценария, значение аргумента)
+     * Например:
+     * если нужно проверить что элемент не отображается на странице, но проверки Selenium отрабатывают неверно,
+     * можно использовать данный метод и проверить, что среди его классов есть disabled
+     * </p>
+     */
+    @Тогда("^элемент \"([^\"]*)\" содержит класс со значением \"(.*)\"$")
+    public void checkElemClassContainsExpectedValue(String elementName, String expectedClassValue) {
+        SelenideElement currentElement = coreScenario.getCurrentPage().getElement(elementName);
+        expectedClassValue = getPropertyOrStringVariableOrValue(expectedClassValue);
+
+        currentElement.shouldHave(
+                attribute("class", expectedClassValue)
+        );
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что элемент не содержит указанный класс
+     * </p>
+     */
+    @Тогда("^элемент \"([^\"]*)\" не содержит класс со значением \"(.*)\"$")
+    public void checkElemClassNotContainsExpectedValue(String elementName, String expectedClassValue) {
+        SelenideElement currentElement = coreScenario.getCurrentPage().getElement(elementName);
+
+        expectedClassValue = getPropertyOrStringVariableOrValue(expectedClassValue);
+
+        currentElement.shouldHave(
+                not(attribute("class", expectedClassValue))
+        );
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что значение в поле содержит значение (в приоритете: из property, из переменной сценария, значение аргумента),
+     * указанное в шаге
+     * </p>
+     */
+    @Тогда("^(?:поле|элемент) \"([^\"]*)\" содержит значение \"(.*)\"$")
+    public void testActualValueContainsSubstring(String elementName, String expectedValue) {
+        expectedValue = getPropertyOrStringVariableOrValue(expectedValue);
+        coreScenario.getCurrentPage().getElement(elementName)
+                .shouldHave(text(expectedValue));
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что значение в поле содержит текст, указанный в шаге
+     * (в приоритете: из property, из переменной сценария, значение аргумента).
+     * Используется метод innerText(), который получает как видимый, так и скрытый текст из элемента,
+     * обрезая перенос строк и пробелы в конце и начале строчки.
+     * Не чувствителен к регистру
+     * </p>
+     */
+    @Тогда("^(?:поле|элемент) \"([^\"]*)\" содержит внутренний текст \"(.*)\"$")
+    public void testFieldContainsInnerText(String elementName, String expectedValue) {
+        expectedValue = getPropertyOrStringVariableOrValue(expectedValue);
+        SelenideElement element = coreScenario.getCurrentPage().getElement(elementName);
+        try {
+            element.shouldHave(text(expectedValue));
+        } catch (ElementShould ex) {
+            element.shouldHave(value(expectedValue));
+        }
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что значение в поле равно значению, указанному в шаге (в приоритете: из property, из переменной сценария, значение аргумента)
+     * </p>
+     */
+    @Тогда("^значение (?:поля|элемента) \"([^\"]*)\" равно \"(.*)\"$")
+    public void compareValInFieldAndFromStep(String elementName, String expectedValue) {
+        expectedValue = getPropertyOrStringVariableOrValue(expectedValue);
+        SelenideElement element = coreScenario.getCurrentPage().getElement(elementName);
+        try {
+            element.shouldHave(text(expectedValue));
+        } catch (ElementShould ex) {
+            element.shouldHave(value(expectedValue));
+        }
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что кнопка/ссылка недоступна для нажатия
+     * </p>
+     */
+    @Тогда("^(?:ссылка|кнопка) \"([^\"]*)\" недоступна для нажатия$")
+    public void buttonIsNotActive(String elementName) {
+        SelenideElement element = coreScenario.getCurrentPage().getElement(elementName);
+        assertTrue(String.format("Элемент [%s] кликабелен", elementName), element.is(Condition.disabled));
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что радиокнопка выбрана
+     * </p>
+     */
+    @Тогда("^радиокнопка \"([^\"]*)\" выбрана$")
+    public void radioButtonIsSelected(String elementName) {
+        SelenideElement element = coreScenario.getCurrentPage().getElement(elementName);
+        element.shouldHave(selected);
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что радиокнопка не выбрана
+     * </p>
+     */
+    @Тогда("^радиокнопка \"([^\"]*)\" не выбрана")
+    public void radioButtonIsNotSelected(String elementName) {
+        SelenideElement element = coreScenario.getCurrentPage().getElement(elementName);
+        element.shouldHave(not(selected));
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что радиокнопка выбрана
+     * </p>
+     */
+    @Тогда("^чекбокс \"([^\"]*)\" выбран$")
+    public void checkBoxIsChecked(String elementName) {
+        SelenideElement element = coreScenario.getCurrentPage().getElement(elementName);
+        element.shouldHave(checked);
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что радиокнопка не выбрана
+     * </p>
+     */
+    @Тогда("^чекбокс \"([^\"]*)\" не выбран$")
+    public void checkBoxIsNotChecked(String elementName) {
+        SelenideElement element = coreScenario.getCurrentPage().getElement(elementName);
+        element.shouldHave(not(checked));
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что поле нередактируемо
+     * </p>
+     */
+    @Тогда("^(?:поле|элемент) \"([^\"]*)\" (?:недоступно|недоступен) для редактирования$")
+    public void fieldIsDisable(String elementName) {
+        SelenideElement element = coreScenario.getCurrentPage().getElement(elementName);
+        assertTrue(String.format("Элемент [%s] доступен для редактирования", elementName), element.is(Condition.disabled));
+    }
+
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка, что на странице не отображаются редактируемые элементы, такие как:
+     * -input
+     * -textarea
+     * </p>
+     */
+    @Тогда("^открыта read-only форма$")
+    public void openReadOnlyForm() {
+        int inputsCount = getDisplayedElementsByCss("input").size();
+        assertTrue("Форма не read-only. Количество input-полей: " + inputsCount, inputsCount == 0);
+        int textareasCount = getDisplayedElementsByCss("textarea").size();
+        assertTrue("Форма не read-only. Количество элементов textarea: " + textareasCount, textareasCount == 0);
+    }
+
+    private List<SelenideElement> getDisplayedElementsByCss(String cssSelector) {
+        return $$(cssSelector).stream()
+                .filter(SelenideElement::isDisplayed)
+                .collect(Collectors.toList());
+    }
+
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Выполняется поиск нужного файла в папке /Downloads
+     * Поиск осуществляется по содержанию ожидаемого текста в названии файла. Можно передавать регулярное выражение.
+     * После выполнения проверки файл удаляется
+     * </p>
+     */
+    @Тогда("^файл \"(.*)\" загрузился в папку /Downloads$")
+    public void testFileDownloaded(String fileName) {
+        File downloads = getDownloadsDir();
+        File[] expectedFiles = downloads.listFiles((files, file) -> file.contains(fileName));
+        assertNotNull("Ошибка поиска файла", expectedFiles);
+        assertFalse("Файл не загрузился", expectedFiles.length == 0);
+        assertTrue(String.format("В папке присутствуют более одного файла с одинаковым названием, содержащим текст [%s]", fileName),
+                expectedFiles.length == 1);
+        deleteFiles(expectedFiles);
+    }
+
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Производится проверка количества символов в поле со значением, указанным в шаге
+     * </p>
+     */
+    @Тогда("^в поле \"([^\"]*)\" содержится (\\d+) символов$")
+    public void checkFieldSymbolsCount(String element, int num) {
+        int length = coreScenario.getCurrentPage().getAnyElementText(element).length();
+        assertEquals(String.format("Неверное количество символов. Ожидаемый результат: %s, текущий результат: %s", num, length), num, length);
+    }
+
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Получение текста элемента в блоке и сохранение его в переменную
+     *
+     * @param elementName  имя элемента
+     * @param blockName    имя блока
+     * @param variableName имя переменной
+     *                     </p>
+     */
+    @Когда("^значение (?:элемента|поля) \"([^\"]*)\" в блоке \"([^\"]*)\" сохранено в переменную \"([^\"]*)\"$")
+    public void saveTextElementInBlock(String elementName, String blockName, String variableName) {
+        String elementText = coreScenario.getCurrentPage().getBlock(blockName).getAnyElementText(elementName);
+        coreScenario.setVar(variableName, elementText);
+        coreScenario.write("Значение [" + elementText + "] сохранено в переменную [" + variableName + "]");
+    }
+
+    /**
+     * <p style="color: green; font-size: 1.5em">
+     * Проверка того, что значение из поля в блоке совпадает со значением заданной переменной из хранилища
+     * </p>
+     *
+     * @param elementName  имя элемента
+     * @param blockName    имя блока
+     * @param variableName имя переменной
+     */
+    @Тогда("^значение (?:поля|элемента) \"([^\"]*)\" в блоке \"([^\"]*)\" совпадает со значением из переменной \"([^\"]*)\"$")
+    public void compareFieldAndVariable(String elementName, String blockName, String variableName) {
+        String expectedValue = coreScenario.getVar(variableName).toString();
+        coreScenario.getCurrentPage().getBlock(blockName).getElement(elementName)
+                .shouldHave(exactText(expectedValue));
+    }
+
+}
