bcd30b05f011019dfa5ebbdeea5b20733b31980f
Petr Molokotin <molokotinpa@msk.bcs.ru>
2019-12-23 17:39:24 +0300

    move javabank to platform

---
+++ at-library-core/src/main/java/ru/bcs/at/library/core/core/javabank/http/responses/Response.java
@@ -0,0 +1,16 @@
+package ru.bcs.at.library.core.core.javabank.http.responses;
+
+import org.json.simple.JSONObject;
+
+import java.util.HashMap;
+
+public abstract class Response extends HashMap {
+
+    public JSONObject getJSON() {
+        return new JSONObject();
+    }
+
+    public String toString() {
+        return getJSON().toJSONString();
+    }
+}
